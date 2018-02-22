25587ef60e88571fb6f47c615f557c3002baba49
Lina Liljenfeldt <lina.liljenfeldt@gmail.com>
2018-02-22 14:59:40 +0100

    Added boarders, new player character

--- src/Player.java
+++ src/Player.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Player extends Character {
 
     public Player(int x, int y){
-        super(x, y, '\u2615');
+        super(x, y, 'P');
     }
     public void moveUp(ArrayList<Gameobject> objs){
         move(0, -1, objs);
